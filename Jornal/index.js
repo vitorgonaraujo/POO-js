438ef905840d4dc22dd785e596b33ef2bd571b96
Vitor <agente51411@gmail.com>
2024-01-09 11:09:44 -0300

    remoção do jornal

--- Jornal/index.js
+++
@@ -1,9 +0,0 @@
-const Jornalista = require("./Jornalista.js");
-const Noticias = require("./Noticias.js");
-
-const junior = new Jornalista("Junior Goulart", "10/09/1989", 12345);
-const News1 = new Noticias(
-  "A queda de Robert Del Miro",
-  "Robert Del Miro, abre no teu cu e atiro, tentou atirar mas não resistiu e morreu.",
-  junior
-);
